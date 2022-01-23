bbcda08c668bafa8b19a1972c84a9b8bc98a77a9
tarik <tarikofkir2017@gmail.com>
2022-01-23 01:41:38 +0100

    iconBrowser

--- src/view/Main.java
+++ src/view/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,6 +17,8 @@ public class Main extends Application {
         TabPane root = FXMLLoader.load( getClass().getResource("/view/fxml/browser.fxml") );
         Scene scene = new Scene( root );
         primaryStage.setScene( scene );
+        primaryStage.setTitle("Browser");
+        primaryStage.getIcons().add(new Image("/view/icons/icons8-browser-60.png"));
         primaryStage.show();
     }
 }
